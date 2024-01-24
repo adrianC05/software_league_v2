26d49813092096bfe5af34a8fe0b912a9f8e7d00
adrianC05 <aacarrion4@espe.edu.ec>
2024-01-23 21:52:49 -0500

    Widgets gastos, ingresos, balance corregidos

--- app/Filament/Widgets/ExpenseOverview.php
+++ app/Filament/Widgets/ExpenseOverview.php
@@ -9,22 +9,30 @@ use Filament\Widgets\StatsOverviewWidget\Stat;
 
 class ExpenseOverview extends BaseWidget
 {
+
     protected function getStats(): array
     {
+        $balance = Revenue::sum('value') - Expense::sum('value');
+        $balanceColor = ($balance >= 0) ? 'success' : 'danger';
         return [
-
-            Stat::make('Total de Gastos', Expense::sum('value'))
-                ->descriptionIcon('heroicon-m-arrow-trending-down')
-                ->color('danger')
-                ->chart(Expense::get()->pluck('value')->toArray()),
-
-            Stat::make('Total de Ingresos', Revenue::sum('value'))
+            Stat::make('Ingresos', '$ ' . number_format(Revenue::sum('value'), 2))
+                ->description('Total de ingresos obtenidos')
                 ->descriptionIcon('heroicon-m-arrow-trending-up')
-                ->color('success')
-                ->chart(Revenue::get()->pluck('value')->toArray()),
+                ->color('success'),
+            //->chart(Revenue::get()->pluck('value')->toArray()),
+
+            Stat::make('Gastos', '$ ' . number_format(Expense::sum('value'), 2))
+                ->description('Total de gastos realizados')
+                ->descriptionIcon('heroicon-m-arrow-trending-down')
+                ->color('danger'),
+            //->chart(Expense::get()->pluck('value')->toArray()),
+
 
             // Gastos - Ingresos
-            Stat::make('Balance',  Revenue::sum('value') - Expense::sum('value')),
+            Stat::make('Balance', '$ ' . number_format($balance, 2))
+                ->description('Balance de ingresos y gastos')
+                ->descriptionIcon('heroicon-m-currency-dollar')
+                ->color($balanceColor),
         ];
     }
 }
