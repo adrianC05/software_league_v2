b54e69ef99238a56e776db065b21fadf36d51b16
AndresssG1 <jachumo@espe.edu.ec>
2024-01-26 07:59:17 -0500

    Refresh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -61,9 +61,38 @@
 
                             <h2 class="mt-6 text-xl font-semibold text-gray-900">TABLA DE POSICIONES</h2>
 
-                            <p class="mt-4 text-gray-500 text-sm leading-relaxed">
-                                Aqui se mostrara las estadisticas de los equipos
-                            </p>
+                            <table class="content-table">
+                                <thead>
+                                    <tr>
+                                        <th>Rank</th>
+                                        <th>Name</th>
+                                        <th>Points</th>
+                                        <th>Team</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Domenic</td>
+                                        <td>88,110</td>
+                                        <td>dcode</td>
+                                    </tr>
+
+
+                                    <tr class="active-row">
+                                        <td>2</td>
+                                        <td>Sally</td>
+                                        <td>72,400</td>
+                                        <td>Students</td>
+                                    </tr>
+                                    <tr>
+                                        <td>3</td>
+                                        <td>Nick</td>
+                                        <td>52,300</td>
+                                        <td>dcode</td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                     <!---------------Segunda tabla--------------------------->
