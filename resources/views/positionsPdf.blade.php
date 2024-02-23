366596ae0b4114f706cd2a39f2ec4b96c318590d
adrianC05 <aacarrion4@espe.edu.ec>
2024-02-22 22:30:11 -0500

    Tamaño de la fuente

--- resources/views/positionsPdf.blade.php
+++ resources/views/positionsPdf.blade.php
@@ -20,6 +20,9 @@
         .img-espe {
             width: 100px;
         }
+        h1 {
+            font-size: 40px;
+        }
     </style>
 </head>
 
--- resources/views/teamsPdf.blade.php
+++ resources/views/teamsPdf.blade.php
@@ -20,6 +20,10 @@
         .img-espe {
             width: 100px;
         }
+
+        h1 {
+            font-size: 40px;
+        }
     </style>
 </head>
 
